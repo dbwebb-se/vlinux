a4dc4dc5ff66f3c525816ee914d7e6e7affa2f52
Kenneth Lewenhagen <lewenhagen@live.com>
2022-06-21 08:33:13 +0200

    something

--- .dbwebb/inspect-src/kmom.d/kmom04/pre.bash
+++ .dbwebb/inspect-src/kmom.d/kmom04/pre.bash
@@ -5,42 +5,36 @@ green=$(tput setaf 2)
 cyan=$(tput setaf 6)
 normal=$(tput sgr 0)
 
-printf ">>> -------------- Pre inspect -------------------------\n"
-
-localport="1335"
-port=$(cat me/kmom04/server/dockerhub.txt | head -n1 | sed 's/.*\([0-9]\{4\}\).*/\1/')
-student_docker=$(cat me/kmom04/server/dockerhub.txt | head -n2 | tail -n1 | rev | sed -E -n 's/^\s?([a-zA-Z0-9]+[:][a-zA-Z0-9\-]+[/][a-zA-Z0-9]+).*/\1/p' | rev)
-
-printf "%s ${cyan}%s${normal}\n" "Using port:" "$localport:$port"
-printf "%s ${cyan}%s${normal}\n" "Docker image:" "$student_docker"
-
-read -p "Press any key to move on."
-
-docker run --rm --name kmom04 -d -p "$localport":"$port" -it -v "$(pwd)"/example/json/:"/server/data/" "$student_docker"
-
-function testServer
+function printthisfile
 {
-    printf "${cyan}\n\n"
-    read -p "Curling localhost:$localport/$1"
-
+    printf "\n${cyan}"
+    more "$1"
     printf "${normal}\n"
-
-    curl "http://localhost:$localport/$1"
-    # tput setaf 6
-    # echo ""
-    # # read -p "Done viewing? <Press Enter>"
-    # tput sgr0
 }
 
-testServer "all"
-testServer "names"
-testServer "color/Yellow"
-testServer "color/yellow"
+cd me/kmom04/ || exit 1
 
-eval "$BROWSER" "http://localhost:$localport/all" &
+printf ">>> -------------- Pre inspect -------------------------\n"
 
-printf "${cyan}\n\n"
+read -p "View dockerhub.bash? [Y/n]" answer
+
+if [[ "$answer" != "n" ]]; then
+    printthisfile "dockerhub.bash"
+fi
+
+read -p "Press any key to execute dockerhub.bash"
+
+mkdir server/temp
+cp ../../example/json/* server/temp/
+
+
+chmod +x dockerhub.bash
+bash dockerhub.bash server/temp
+
+eval "$BROWSER" "http://localhost:8080" &
+
 read -p "[CHECK BROWSER] Press any key to continue."
-printf "${normal}"
 
-# docker kill "$dockerId"
+# read -p "Press any key to delete the image."
+#
+# docker images -a |  grep "vlinux-vhost" | awk '{print $3}' | xargs docker rmi -f
